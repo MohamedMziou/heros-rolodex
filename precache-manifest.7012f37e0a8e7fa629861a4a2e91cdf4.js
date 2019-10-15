c6701d8e811575dab8bc1919fa87e41e5988266e
wassimcharbti <wassim.charbti53@gmail.com>
2019-10-15 16:40:12 +0100

    Updates

---
+++ precache-manifest.7012f37e0a8e7fa629861a4a2e91cdf4.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a164384d9bc2c6652791c6458f096356",
+    "url": "/heros-rolodex/index.html"
+  },
+  {
+    "revision": "309bed08ac8ff617840b",
+    "url": "/heros-rolodex/static/css/main.49d2a6d9.chunk.css"
+  },
+  {
+    "revision": "b0703eff4582a78f493a",
+    "url": "/heros-rolodex/static/js/2.5075ebff.chunk.js"
+  },
+  {
+    "revision": "309bed08ac8ff617840b",
+    "url": "/heros-rolodex/static/js/main.09708309.chunk.js"
+  },
+  {
+    "revision": "08654fbb4cf0b90dc586",
+    "url": "/heros-rolodex/static/js/runtime-main.ff75bf03.js"
+  }
+]);
